576107236fb8b74d55101312e3f487c906a4b69c
ttk <ttk7813@gmail.com>
2021-01-31 22:23:06 +0900

    :gear: 新しいTODOを作るページの雛形作成

---
+++ src/pages/todo/_components/Editor.svelte
@@ -0,0 +1,7 @@
+<script>
+  export let title;
+  export let content;
+</script>
+
+<input type="text" placeholder="タイトル" bind:value={title}>
+<textarea placeholder="内容" bind:value={content}></textarea>
--- src/pages/todo/new.svelte
+++ src/pages/todo/new.svelte
@@ -1,1 +1,15 @@
-<p>新しいTODOを作成するページ</p>
+<script>
+  import { url } from '@roxi/routify'
+  import Editor from './_components/Editor.svelte';
+  import { addTodo } from './_components/assets/storage';
+  let title = '';
+  let content = '';
+  const onSave = () => {
+    addTodo({title, content});
+  }
+</script>
+
+<Editor bind:title={title} bind:content={content}></Editor>
+
+<button onclick="location.href={$url('./index')}" on:click={onSave}>保存</button>
+<button onclick="location.href={$url('./index')}">戻る</button>
